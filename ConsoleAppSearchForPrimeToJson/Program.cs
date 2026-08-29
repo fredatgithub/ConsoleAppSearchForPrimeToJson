d86a05c74dcb1c45b8e43c8b4cfc7d1bf3c84b8d
fred <domaine75@gmail.com>
2026-08-29 11:57:25 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleAppSearchForPrimeToJson/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppSearchForPrimeToJson
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("Calcul des nombres premiers et enregistrement dans un fichier JSON");
+
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
