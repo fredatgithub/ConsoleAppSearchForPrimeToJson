33e983427cc74c39533d6e3b0592da1a8b282e70
fred <domaine75@gmail.com>
2026-08-29 12:02:04 +0200

    add prime class

---
+++ ConsoleAppSearchForPrimeToJson/PrimeToJson.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppSearchForPrimeToJson
+{
+  public class PrimeToJson
+  {
+    public DateTime StartCalculationDate { get; set; }
+    public DateTime EndCalculationDate { get; set; }
+    public TimeSpan CalculationDuration { get; set; }
+
+    public ulong NumberOfPrimes { get; set; } = 0;
+    public List<ulong> Primes { get; set; } = [];
+
+    public PrimeToJson() { }
+  }
+}
