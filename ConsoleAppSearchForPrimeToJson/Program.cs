3eb9d5a4038f45cdccafcab338c43d532cf51223
fred <domaine75@gmail.com>
2026-08-31 10:59:17 +0200

    add code

--- ConsoleAppSearchForPrimeToJson/Program.cs
+++ ConsoleAppSearchForPrimeToJson/Program.cs
@@ -6,7 +6,9 @@
     {
       Action<string> display = Console.WriteLine;
       display("Calcul des nombres premiers et enregistrement dans un fichier JSON");
-
+      string fileName = "primes.json";
+      string json = File.Exists(fileName) ? File.ReadAllText(fileName) : string.Empty;
+      string currentFileName = "primes1.json";
 
       display("Press any key to exit:");
       Console.ReadKey();
